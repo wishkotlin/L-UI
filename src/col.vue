606af30e0f925be0cec40a824014e168fb54ff79
liulinboyi <814921718@qq.com>
2019-05-12 22:58:05 +0800

    完成 Grid基本功能功能

--- src/col.vue
+++ src/col.vue
@@ -1,18 +1,48 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[ span && `col-${span}`,offset && `col-offset-${offset}`]"
+    :style="{padding: `0 ${gutter}px`}">
+        <div class="gutter-box">
         <slot>
 
         </slot>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "g-col",
+        data(){
+            return{
+                gutter: this.$props.gutter
+            }
+        },
         props: {
             span: {
-                type: [Number,String] ,
-            }
+                type: [Number, String],
+            },
+            offset: {
+                type: [Number, String],
+            },
+            // gutter: {
+            //     type: [Number,String],
+            // }
+        },
+        computed: {
+            // gutter: function(){
+            //     return this.$props.gutter
+            // }
+            // gutter: {
+            //     get: function () {
+            //         return this.$props.gutter
+            //     },
+            //     set: function (newValue) {
+            //         this.$props.gutter = newValue
+            //     }
+            // }
+        },
+        mounted() {
+
         }
     }
 </script>
@@ -20,18 +50,28 @@
 <style lang="scss" scoped>
     .col {
         height: 100px;
-        background: aqua;
+        /*background: aqua;*/
         width: 50%;
-        outline: 1px red solid;
-
+        /*outline: 1px red solid;*/
+        /*border: 1px solid red;*/
+        //间隙
+        /*padding: 0 10px;*/
         $class-prefix: col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: calc(100% * #{$n} / 24);
             }
-            .col[data-col='2'] {
-                width: calc(100% * 2 / 24);
+        }
+        $class-prefix-offset: col-offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix-offset}#{$n} {
+                margin-left: calc(100% * #{$n} / 24);
             }
+        }
+        .gutter-box{
+            outline: 1px solid red;
+            background: rgb(76, 188, 240);
+            height: 100px;
         }
     }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,41 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{margin: `0 -${gutter}px`}">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "g-row"
+        name: "g-row",
+        props: {
+            gutter: {
+                type: [Number, String],
+            }
+        },
+        /**
+         * created 创建组件
+         */
+        created() {
+
+        },
+        /**
+         * mounted 将组件放到页面上
+         */
+        mounted() {
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+
     .row {
         display: flex;
+        //margin: 0 -10px;
+        /*::v-deep .col {*/
+        /*    padding: 0 10px;*/
+        /*}*/
     }
 </style>
