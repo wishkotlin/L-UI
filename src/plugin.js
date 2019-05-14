10c790e780bfc0d8b6f7953167746f4b16c99a69
liulinboyi <814921718@qq.com>
2019-05-14 14:40:57 +0800

    实现了页面只有一个Toast

--- src/plugin.js
+++ src/plugin.js
@@ -1,17 +1,32 @@
 import Toast from './toast'
+
+let currentToast
 export default {
-    install(Vue,options){
-        Vue.prototype.$toast = function(msg,Toastoptions){
+    install(Vue, options) {
+        Vue.prototype.$toast = function (msg, Toastoptions) {
             // let { msg,closeButton,autoClose,autoCloseDelay } = option
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData: {
-                    ...Toastoptions
-                }
-            })
-            toast.$slots.default = [msg]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            if (currentToast) {
+                currentToast.close()
+            }
+
+            function CreateToast() {
+                //创建 Toast组件实例
+                let Constructor = Vue.extend(Toast)
+                let toast = new Constructor({
+                    //组件实例中传入 props
+                    propsData: {
+                        ...Toastoptions
+                    }
+                })
+                toast.$slots.default = [msg]
+                toast.$mount()
+                document.body.appendChild(toast.$el)
+                //将 toast实例赋值给 变量 currentToast
+                // 用来保证页面只有一个Toast实例
+                currentToast = toast
+            }
+
+            CreateToast()
             // console.log(msg)
         }
     }
