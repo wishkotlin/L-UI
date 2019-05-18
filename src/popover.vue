28c76ce4fbaf955449932cb8a21e672d6d6c2f62
liulinboyi <814921718@qq.com>
2019-05-18 14:45:11 +0800

    解决父元素overflowhidden元素的影响 以及 window.scrolX和window.scrollY

--- src/popover.vue
+++ src/popover.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="popover">
-        <div class="content-wrapper" v-if="visiable" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visiable" @click.stop>
             <slot name="content"></slot>
         </div>
-        <div @click="xxx">
+        <span @click="xxx" ref="triggerWapper">
             <slot name="trigger"></slot>
-        </div>
+        </span>
     </div>
 </template>
 
@@ -17,6 +17,9 @@
                 visiable: false
             }
         },
+        mounted() {
+            console.log(this.$refs.triggerWapper);
+        },
         methods: {
             async xxx(event) {
                 // console.log(event)
@@ -27,13 +30,18 @@
                 this.visiable = !this.visiable
                 if (this.visiable === true) {
                     this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width, height, top, left} = this.$refs.triggerWapper.getBoundingClientRect()
+                        console.log(width, height, top, left)
+                        this.$refs.contentWrapper.style.left = `${left+window.scrollX}px`
+                        this.$refs.contentWrapper.style.top = `${top+window.scrollY}px`
                         //() => {}
                         //function(){}.bind(this) bind() 会生成 新的函数
                         let handler = () => {
                             this.visiable = false
-                            document.removeEventListener('click',handler)
+                            document.removeEventListener('click', handler)
                         }
-                        document.addEventListener('click',handler)
+                        document.addEventListener('click', handler)
                     })
                 }
             }
@@ -47,13 +55,13 @@
         vertical-align: top;
         position: relative;
 
-        .content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px red solid;
-            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        }
+    }
+
+    .content-wrapper {
+        position: absolute;
+        border: 1px red solid;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%);
     }
 
 </style>
